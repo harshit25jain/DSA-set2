500274e5c20479fb83e79c349bb9d57432a3897d
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-24 20:39:58 +0530

    Update longest-palindrome.cpp

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -5,9 +5,9 @@ public:
      for(char c:s) a[c]++;  
      int ans=0,k=0;
      for(auto i:a){
-        if(i.second%2==0) ans+=i.second;
+        if(i.second%2==0) ans+=i.second; //when count of char is even
         else {
-            ans+=i.second-1;
+            ans+=i.second-1; //when count of char is odd
             k=1;}}
      if(k==1) ans++;
      return ans;
