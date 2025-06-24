7426be7daddd7ad836f8d6990353219766aec7a7
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-25 01:33:54 +0530

    Update longest-continuous-increasing-subsequence.cpp

--- 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.cpp
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.cpp
@@ -12,3 +12,12 @@ public:
         return ans;
     }
 };
+/*
+1.Start with a count = 1 because any non-empty array has at least a length-1 subsequence.
+2.Use a loop starting from the second element.
+3.For each element:
+If it's greater than the previous one, increment the current count.
+If not, reset the count to 1.
+
+4.Track the maximum length encountered using ans.
+5.Return the maximum found.*/
