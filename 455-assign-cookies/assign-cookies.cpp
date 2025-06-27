22c102ca93071e9f7f561416d7565beb01649b47
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-06-27 18:28:10 +0530

    Update assign-cookies.cpp

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -21,3 +21,12 @@ public:
         return r;
     }
 };
+
+/* O(nlogn) since we sorted the arrays
+a) sort both the arrays in ascending order
+b) traverse both of them using 2 pointers.
+c) the right pointer is moved if the greed is satisfied by a cookie, but the left pointer moves regardless
+this is true because if the left pointer cannot satisfy greed of lets say g[j], it wont satisfy g[j+1]
+either since its sorted. at the end, return the right pointer
+*/
+
