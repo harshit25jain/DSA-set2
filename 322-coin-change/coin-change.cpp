f01c375fbd95caabe3a9f7e29250a70da56b09ad
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-07-02 22:43:40 +0530

    Update coin-change.cpp

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -13,3 +13,12 @@ public:
         return dp[amount] == INF ? -1 : dp[amount];
     }
 };
+/*
+We use a 1D DP array dp where dp[i] stores the minimum number of coins needed to form amount i.
+Initialize dp[0] = 0 since 0 coins are needed to make amount 0.
+Set all other dp[i] values to a large number (amount + 1), effectively "infinity".
+For each coin, iterate from that coin's value up to amount:
+For each i, try using the current coin: dp[i] = min(dp[i], dp[i - coin] + 1)
+This means: To form amount i, take the best between the existing value and using one more of the current coin.
+After processing, if dp[amount] is still INF, it's not possible to make the amount; return -1. Otherwise, return dp[amount].
+*/
