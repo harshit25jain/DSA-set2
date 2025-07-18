b712363d195cbd5b523a0cc037d253be8aa3a5c5
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-07-18 22:27:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (85.77%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        int n = digits.size();
+        vector<int>ans;
+        vector<int>mpp(10,0);
+
+        for(auto it : digits){
+            mpp[it]++;
+        }
+
+        for(int i = 1 ; i <= 9 ; i++){
+            if(mpp[i] == 0)continue;
+            mpp[i]--;
+            for(int j = 0 ; j <= 9 ; j++){
+                if(mpp[j] == 0)continue;
+                   mpp[j]--;
+                for(int k = 0 ; k <= 8 ; k+=2){
+                    if(mpp[k] == 0)continue;
+                       mpp[k]--;
+                       ans.push_back(i*100 + j * 10 + k);
+                       mpp[k]++;
+                }
+                mpp[j]++;
+            }
+            mpp[i]++;
+        }
+        return ans;
+
+    }
+};
