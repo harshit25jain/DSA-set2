0afe29cd5648c7aa656d472f6cc44540875ca1b5
Harshit Jain <147839920+harshit25jain@users.noreply.github.com>
2025-07-21 19:04:26 +0530

    Update candy.cpp

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -30,3 +30,31 @@ public:
         return candy;
     }
 };
+/*
+Let take the Rating as : [1,3,6,8,9,5,3,6,8,5,4,2,2,3,7,7,9,8,6,6,6,4,2]
+
+Each child represented as rating(candy he is given)
+Peak = max(peak, valley)
+
+See when peak is encountered we take max of the peak calculated from left and valley calculated from right.
+When we get any equal element it gets reset to 1 candy or if it is peak we take max(0, right valley)
+
+           (5)         (4)                         (3)
+            9           8                           9
+           /|\         /|\                         /|\
+          / | \       / | \                (3)    / | \
+      (4)8  |  5(2)  6  |  5(3)             7 __ 7  |  8(2)
+        /   |   \   (2) |   \              /|   (1) |   \
+       /    |    \ /    |    \            / |    |  |    \         (3)
+   (3)6     |     3     |     4(2)       3(2)    |  |     6 __ 6 __ 6    -> Total candy = 50
+     /      |    (1)    |      \        /   | Reset |    (1)  (1)   |\
+    /       |           |       \      /    |  to 1 |          |    | \
+(2)3        |           |        2 __ 2     |       |          |    |  4(2)
+  /         |           |       (1)  (1)    |       |        Reset  |   \
+ /          |           |                   |       |         to 1  |    \
+1(1)        |           |                   |       |               |     2(1)
+   Peak= max(5,3)  Peak= max(3,4)    Peak= max(3,0) |         Peak= max(0,3)
+                                               Peak= max(2,3)   
+
+*/
+
